b77ee91442771581c98eb8c81fd76033cd057505
shenqichao <shenqichao@pwrd.com>
2025-06-27 16:17:11 +0800

    qx day4

--- src/main/kotlin/tasks/huodong/HuodongUtil.kt
+++ src/main/kotlin/tasks/huodong/HuodongUtil.kt
@@ -9,6 +9,7 @@ import tasks.huodong.qiuxiang.QiuXiangDay8
 import tasks.huodong.qiuxiang2.QiuXiang2Day1
 import tasks.huodong.qiuxiang2.QiuXiang2Day2
 import tasks.huodong.qiuxiang2.QiuXiang2Day3
+import tasks.huodong.qiuxiang2.QiuXiang2Day4
 import tasks.huodong.sanguo.*
 import tasks.huodong.shuihu.*
 import tasks.huodong.shuihu3.*
@@ -55,6 +56,7 @@ object HuodongUtil {
             0->QiuXiang2Day1()
             1->QiuXiang2Day2()
             2-> QiuXiang2Day3()
+            3-> QiuXiang2Day4()
             else->null
         }
     }
