5b0f47eab5dd54c58e60fdc9227a69f6f6ef8130
shenqichao <shenqichao@pwrd.com>
2025-09-23 20:04:16 +0800

    副卡

--- src/main/kotlin/tasks/huodong/fengbao/BaseFengbao.kt
+++ src/main/kotlin/tasks/huodong/fengbao/BaseFengbao.kt
@@ -1,59 +1,9 @@
 package tasks.huodong.fengbao
 
-import data.HeroBean
-import data.HeroCreator
 import tasks.SimpleHeZuoHeroDoing
 
-class Fengbao1 : SimpleHeZuoHeroDoing() {
+abstract class BaseFengbao:SimpleHeZuoHeroDoing() {
 
-    val tieqi = HeroCreator.dianfa.create()
-    val tianshi = HeroCreator.tianshi.create()
-    val yuren = HeroCreator.yuren.create()
-    val gugu = HeroCreator.gugu.create()
-    val shengqi = HeroCreator.shengqi.create()
-    val saman2 = HeroCreator.saman2.create()
-    val wangjiang2 = HeroCreator.wangjiang2.create()
-    val bingqiu = HeroCreator.bingqiu.create()
-    val xiaoye = HeroCreator.xiaoye.create()
-    val shexian = HeroCreator.shexian.create()
-    override fun initHeroes() {
-        super.initHeroes()
-        guanDealList.add(GuanDeal(
-            0,
-            isOver = {
-                fulls(tieqi, tianshi, yuren, gugu, saman2, xiaoye, shexian)
-            },
-            chooseHero = {
-                upAny(tieqi, tianshi, yuren, gugu, saman2, xiaoye, shexian)
-            }
-        ))
 
-        guanDealList.add(GuanDeal(
-            90,
-            isOver = {
-                wangjiang2.isInCar()
-            },
-            chooseHero = {
-                upAny(wangjiang2)
-            },
-            onGuanDealStart = {
-                carDoing.downHero(saman2)
-            }
-        ))
-
-        guanDealList.add(GuanDeal(
-            100,
-            isOver = {
-                fulls(tieqi, tianshi, yuren, gugu, saman2, xiaoye, shexian)
-            },
-            chooseHero = {
-                upAny(tieqi, tianshi, yuren, gugu, saman2, xiaoye, shexian)
-            }, onGuanDealStart = {
-                carDoing.downHero(wangjiang2)
-            }
-        ))
-
-        curGuanDeal = guanDealList.get(0)
-    }
 
 }
