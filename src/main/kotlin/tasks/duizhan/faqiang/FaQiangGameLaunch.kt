853ac58db23e7b6bdb0e0c0079718c1331f6f10e
shenqichao <shenqichao@pwrd.com>
2026-03-10 12:33:37 +0800

    关卡识别频率降低

--- src/main/kotlin/tasks/duizhan/faqiang/FaQiangGameLaunch.kt
+++ src/main/kotlin/tasks/duizhan/faqiang/FaQiangGameLaunch.kt
@@ -251,6 +251,8 @@ class FaQiangGameLaunch : IGameLaunch {
                 Config.adv_close.click()
                 delay(100)
                 Config.adv_close2.click()
+                delay(100)
+                Config.adv_close3.click()
             }
             delay(3000)
             MPoint(100, 130).click()
